25f7cb41fdbbe1104d4a7b81d30e06c5c65b93b2
salehAbdessalem <salih.abdessalem@gmail.com>
2024-09-27 14:53:27 +0100

    01-Create-Next-App-Layout

---
+++ components/header/header.tsx
@@ -0,0 +1,27 @@
+import Link from 'next/link'
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+        <nav>
+            <div className='navbar justify-between bg-base-300'>
+                <Link href="/" className='btn btn-ghost text-lg'>
+                    Next Amazona V2
+                </Link>
+
+                <ul className='flex'>
+                    <li>
+                        <Link href="/cart" className='btn btn-ghost rounded-btn'>Cart</Link>
+                    </li>
+                    <li>
+                        <Link href="/signin" className='btn btn-ghost rounded-btn'>Sign In</Link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+  )
+}
+
+export default Header
